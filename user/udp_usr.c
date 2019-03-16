5880dd284b51814725675fc9181e4e083eff141b
pinkasel <pinkasel@xdp-node.user-ex.cu-bison-lab-pg0.utah.cloudlab.us>
2019-03-15 19:44:57 -0600

    compilation sucess

--- user/udp_usr.c
+++ user/udp_usr.c
@@ -9,7 +9,7 @@ int main()
 
 	struct bpf_prog_load_attr prog_load_attr = {
 		.prog_type = BPF_PROG_TYPE_XDP,
-		.file = "../kernel/udp_kern.o",
+		.file = "udp_kern.o",
 	};
 
 	if(bpf_prog_load_xattr(&prog_load_attr, &obj, &prog_fd))
